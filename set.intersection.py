9d6bfdcc47e28d09ab960ade620916cf6d405045
Balla Kavya <ballakavya594@gmail.com>
2024-11-23 20:20:15 +0530

    set.intersection.py

---
+++ set.intersection.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+m=input()
+ms=set(map(int,input().split()))
+n=input()
+ns=set(map(int,input().split()))
+print(len(ms.intersection(ns)))
