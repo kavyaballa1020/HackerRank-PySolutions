25569b0535dba2da6cb79b8674fde92f340840db
Balla Kavya <ballakavya594@gmail.com>
2024-11-25 12:42:27 +0530

    Runner-up.py

--- Runner-up.py
+++ Runner-up.py
@@ -8,3 +8,11 @@ if __name__ == '__main__':
             newarr.append(i)         
     x=max(newarr)
     print(x)
+
+
+
+# n=int(input())
+#     ls1=list(map(int,input().split()))
+#     ls2=list(set(ls1))
+#     ls2.sort()
+#     print(ls2[-2])
