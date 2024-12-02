8a1f82298eff24a41d2f5f048e31d573314bdf1e
Balla Kavya <ballakavya594@gmail.com>
2024-12-02 12:44:22 +0530

    zeros and ones.py

---
+++ zeros and ones.py
@@ -0,0 +1,5 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import numpy as np
+arr=list(map(int,input().split()))
+print(np.zeros((arr),dtype=np.int))
+print(np.ones((arr),dtype=np.int))
